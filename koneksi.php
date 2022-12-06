88febd5fb826c06ab04b9d55007dfaf030cc0e9a
Toby Manurung <tobymanurung01@gmail.com>
2022-12-06 15:38:59 +0700

    first commit

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,6 @@
 <?php 
 
-$koneksi = mysqli_connect("localhost","root","","sriwedar_evote");
+$koneksi = mysqli_connect("localhost","root","","databasePemira2022");
 
 if (mysqli_connect_error()){
 	echo "koneksi database gagal" .mysqli_connect_error();
